4ceef76cea7d4da24a74617075d0edb5e5bd0ee5
Ilya <ilya.myacghenko.96@mail.ru>
2022-12-20 21:36:07 +0500

    написал документацию docsting

--- 2.1.3.py
+++ 2.1.3.py
@@ -7,6 +7,7 @@ from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Font, Border, Side
 
+
 def get_salary_avg(dct):
     for key, value in dct.items():
         dct[key] = 0 if len(value) == 0 else int(sum(value) / len(value))
@@ -14,10 +15,33 @@ def get_salary_avg(dct):
 
 
 class Vacancy:
+    """
+    Класс представления параметров вакансии
+
+    Attributes:
+        currency_to_rub (dict) : Словарь для перевода оклада в рубли
+        name (str): Название вакансии
+        salary_from (str / int / float): Нижняя граница вилки оклада
+        salary_to (str / int / float): Верхняя граница вилки оклада
+        salary_currency (str): Код валюты оклада
+        area_name (str): Регион
+        published_at (str): Дата публикации вакансии
+
+    """
     currency_to_rub = {"AZN": 35.68, "BYR": 23.91, "EUR": 59.90, "GEL": 21.74, "KGS": 0.76,
                        "KZT": 0.13, "RUR": 1, "UAH": 1.64, "USD": 60.66, "UZS": 0.0055}
 
     def __init__(self, dct):
+        """Инициализатор класса Vacancy
+        :param self:
+            dct (dict):
+                dct['name'] (str): Название вакансии
+                dct['salary_from'] (str / int / float): Нижняя граница вилки оклада
+                dct['salary_to'] (str / int / float): Верхняя граница вилки оклада
+                dct['salary_currency'] (str): Код валюты оклада
+                dct['area_name'] (str): Регион
+                dct['published_at'] (str): Дата публикации вакансии
+        """
         self.name = dct['name']
         self.salary_from = float(dct['salary_from'])
         self.salary_to = float(dct['salary_to'])
@@ -26,18 +50,48 @@ class Vacancy:
         self.published_at = dct['published_at']
 
     def get_average_rub_salary(self):
+        """Метод возвращает среднюю зарплату оклада в рублях
+
+        :return:
+            (float): Средняя зарплата оклада в рублях
+        """
         return 0.5 * (self.salary_from + self.salary_to) * self.currency_to_rub[self.salary_currency]
 
     def get_published_vacancy_year(self):
+        """Возвращает год публикации вакансии
+        :return:
+        (int): Год публикации вакансии
+        """
         return int(self.published_at[:4])
 
 
 class DataSet:
+    """Класс для представления данных о вакансиях
+    :Attributes:
+        file_name (str): Название файла для анализа
+        vacancy_name (str): Название выбранной вакансии
+    """
     def __init__(self, file_name, name):
+        """Инициализирует клас DataSet
+        :param file_name:
+            (str): Название файла
+        :param name:
+            (str): Название выбранной вакансии
+        """
         self.file_name = file_name
         self.vacancy_name = name
 
     def parse_csv(self):
+        """Парсит сырые данных из файла по словарям
+        :return:
+            salary (dict): Зарплата по годам
+            amount (dict): Количество вакансий по годам
+            vacancy_salary (dict): Зарплата по годам для выбранной вакансии
+            vacancy_amount (dict): Количество вакансий по годам для выбранной вакансии
+            salary_city (dict): Зарплата по городам
+            share_city(dict): Количество по городам
+            count (int): Общее количество всех вакансий
+        """
         salary = dict()
         amount = dict()
         vacancy_salary = dict()
@@ -89,6 +143,15 @@ class DataSet:
             return salary, amount, vacancy_salary, vacancy_amount, salary_city, share_city, count
 
     def get_clear_data(self):
+        """Преобразует сырые данные из метода parse_csv
+        :return:
+            salary (dict): Средняя зарплата по годам
+            amount (dict): Количество вакансий по годам
+            vacancy_salary (dict): Средняя зарплата по годам для выбранной вакансии
+            vacancy_amount (dict): Количество вакансий по годам для выбранной вакансии
+            salary_city (dict): Зарплата по городам (В порядке убывания)
+            share_city(dict): Доля вакансий по городам (В порядке убывания)
+        """
         salary, amount, vacancy_salary, vacancy_amount, salary_city, share_city, count = self.parse_csv()
         salary = get_salary_avg(salary)
         vacancy_salary = get_salary_avg(vacancy_salary)
@@ -105,7 +168,34 @@ class DataSet:
 
 
 class Report:
+    """Класс для представления отчётов
+    Attributes:
+        wb (Workbook): Workbook is the container for all other parts of the document
+        vacancy_name (str): Название выбранной вакансии
+        salary (dict): Средняя зарплата по годам
+        amount (dict): Количество вакансий по годам
+        this_vacancy_salary (dict): Средняя зарплата по годам для выбранной вакансии
+        this_vacancy_amount (dict): Количество вакансий по годам для выбранной вакансии
+        salary_city (dict): Зарплата по городам (В порядке убывания)
+        share_city(dict): Доля вакансий по городам (В порядке убывания)
+    """
     def __init__(self, vacancy_name, salary, amount, this_vacancy_salary, this_vacancy_amount, salary_city, share_city):
+        """Инициализирует класс Report
+        :param:
+            vacancy_name (str): Название выбранной вакансии
+        :param:
+            salary (dict): Средняя зарплата по годам
+        :param:
+            amount (dict): Количество вакансий по годам
+        :param:
+            this_vacancy_salary (dict): Средняя зарплата по годам для выбранной вакансии
+        :param:
+            this_vacancy_amount (dict): Количество вакансий по годам для выбранной вакансии
+        :param:
+            salary_city (dict): Зарплата по городам (В порядке убывания)
+        :param:
+            share_city(dict): Доля вакансий по городам (В порядке убывания)
+        """
         self.wb = Workbook()
         self.vacancy_name = vacancy_name
         self.salary = salary
@@ -116,6 +206,7 @@ class Report:
         self.share_city = share_city
 
     def generate_image(self):
+        """Формирует отчет в виде graph.png с графиками"""
         fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2)
 
         bar1 = ax1.bar(np.array(list(self.salary.keys())) - 0.4, self.salary.values(), width=0.4)
@@ -153,6 +244,7 @@ class Report:
         plt.savefig('graph.png')
 
     def generate_pdf(self):
+        """Формирует отчет в виде pdf-файла со статистикой и графиками"""
         template = Environment(loader=FileSystemLoader('templates')).get_template("pdf.html")
         statistic = []
         for year in self.salary:
@@ -168,6 +260,7 @@ class Report:
         pdfkit.from_string(pdf, 'report.pdf', configuration=config, options={"enable-local-file-access": ""})
 
     def generate_excel(self):
+        """Формирует статистику в виде .xlsx"""
         ws1 = self.wb.active
         ws1.title = 'Статистика по годам'
         ws1.append(['Год', 'Средняя зарплата', 'Средняя зарплата - ' + self.vacancy_name, 'Количество вакансий',
